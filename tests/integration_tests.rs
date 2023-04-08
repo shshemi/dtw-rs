97d747e09aff962ddc8e584d7d26094113b6e97f
Shayan Hashemi <shshemi@gmail.com>
2023-04-09 00:12:03 +0300

    update tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,4 +1,6 @@
-use dtw_rs::{Distance, DynamicTimeWarp};
+use dtw_rs::{Distance, DynamicTimeWarping, dynamic_programming::DynamicProgramming, DyanmicTimeWarpingAlgorithm};
+use std::default::Default;
+
 
 #[test]
 fn end_to_end_f64() {
@@ -6,7 +8,7 @@ fn end_to_end_f64() {
     let b = [2.0, 0.0, 0.0, 8.0, 7.0, 2.0].map(MockF64);
     let expected_path = [(0, 0), (0, 1), (1, 2), (2, 3), (2, 4), (3, 5), (4, 5)];
     let expected_distance = 9.0;
-    let dtw = DynamicTimeWarp::between(&a, &b);
+    let dtw = DynamicProgramming::between(&a, &b);
     println!("Matrix:");
     println!("{}", dtw);
     println!("Path: {:?}", dtw.path());
@@ -16,15 +18,9 @@ fn end_to_end_f64() {
 
 #[test]
 fn end_to_end_char() {
-    let a = "abbc"
-        .chars()
-        .map(MockChar)
-        .collect::<Vec<MockChar>>();
-    let b = "abc"
-        .chars()
-        .map(MockChar)
-        .collect::<Vec<MockChar>>();
-    let dtw = DynamicTimeWarp::between(&a, &b);
+    let a = "abbc".chars().map(MockChar).collect::<Vec<MockChar>>();
+    let b = "abc".chars().map(MockChar).collect::<Vec<MockChar>>();
+    let dtw = DynamicProgramming::between(&a, &b);
     println!("Matrix:");
     println!("{}", dtw);
     println!("Path: {:?}", dtw.path());
