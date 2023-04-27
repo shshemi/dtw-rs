11390e3ba0bafcce45f22337f375428ba2f24154
Shayan Hashemi <shshemi@gmail.com>
2023-04-28 00:37:31 +0300

    remove character tests as it seems meaning less

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -16,19 +16,7 @@ fn dynamic_programming_distance_trait() {
 }
 
 #[test]
-fn end_to_end_char() {
-    let a = "abbc".chars().map(MockChar).collect::<Vec<MockChar>>();
-    let b = "abc".chars().map(MockChar).collect::<Vec<MockChar>>();
-    let dtw = DynamicProgramming::between(&a, &b);
-    println!("Matrix:");
-    println!("{}", dtw);
-    println!("Path: {:?}", dtw.path());
-    assert!(dtw.distance() == 0.0);
-    assert!(*dtw.path() == [(0, 0), (1, 1), (2, 1), (3, 2)]);
-}
-
-#[test]
-fn end_to_end_f64_with_absolute_distance() {
+fn dynamic_programming_absolute_distance() {
     let a = [1.0, 3.0, 9.0, 2.0, 1.0];
     let b = [2.0, 0.0, 0.0, 8.0, 7.0, 2.0];
     let expected_path = [(0, 0), (0, 1), (1, 2), (2, 3), (2, 4), (3, 5), (4, 5)];
@@ -59,7 +47,6 @@ fn dynamic_programming_custom_distance() {
 
 
 struct MockF64(f64);
-struct MockChar(char);
 
 impl Distance for MockF64 {
     fn distance(&self, other: &Self) -> f64 {
@@ -67,12 +54,3 @@ impl Distance for MockF64 {
     }
 }
 
-impl Distance for MockChar {
-    fn distance(&self, other: &Self) -> f64 {
-        if self.0 == other.0 {
-            0.0
-        } else {
-            1.0
-        }
-    }
-}
