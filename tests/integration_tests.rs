5336190675bd53d8e92c92971e4ced838bea3dbc
Shayan Hashemi <shshemi@gmail.com>
2023-05-18 15:20:17 +0300

    add test for dynamic programming with band restriction

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,4 +1,7 @@
-use dtw_rs::{methods::DynamicProgramming, DynamicTimeWarping, Distance};
+use dtw_rs::{
+    methods::DynamicProgramming, methods::Restriction, Distance, DynamicTimeWarping,
+    ParameterizedDynamicTimeWarping,
+};
 
 #[test]
 fn dynamic_programming_distance_trait() {
@@ -38,6 +41,41 @@ fn dynamic_programming_custom_distance() {
     let expected_distance = 9.0;
 
     let dtw = DynamicProgramming::with_closure(&a, &b, |a, b| f64::abs(a - b));
+    println!("Matrix:");
+    println!("{}", dtw);
+    println!("Path: {:?}", dtw.path());
+    assert!(dtw.distance() == expected_distance);
+    assert!(*dtw.path() == expected_path);
+}
+
+#[test]
+fn dynamic_programming_band_restricted_distance_trait() {
+    let a = [1.0, 3.0, 9.0, 2.0, 1.0].map(MockF64);
+    let b = [2.0, 0.0, 0.0, 8.0, 7.0, 2.0].map(MockF64);
+    let expected_path = [(0, 0), (0, 1), (1, 2), (2, 3), (2, 4), (3, 5), (4, 5)];
+    let expected_distance = 9.0;
+
+    let dtw = DynamicProgramming::with_hyper_parameters(&a, &b, Restriction::Band(1));
+    println!("Matrix:");
+    println!("{}", dtw);
+    println!("Path: {:?}", dtw.path());
+    assert!(dtw.distance() == expected_distance);
+    assert!(*dtw.path() == expected_path);
+}
+
+#[test]
+fn dynamic_programming_band_restricted_custom_distance() {
+    let a = [1.0, 3.0, 9.0, 2.0, 1.0];
+    let b = [2.0, 0.0, 0.0, 8.0, 7.0, 2.0];
+    let expected_path = [(0, 0), (0, 1), (1, 2), (2, 3), (2, 4), (3, 5), (4, 5)];
+    let expected_distance = 9.0;
+
+    let dtw = DynamicProgramming::with_closure_and_hyper_parameters(
+        &a,
+        &b,
+        |a, b| f64::abs(a - b),
+        Restriction::Band(1),
+    );
     println!("Matrix:");
     println!("{}", dtw);
     println!("Path: {:?}", dtw.path());
